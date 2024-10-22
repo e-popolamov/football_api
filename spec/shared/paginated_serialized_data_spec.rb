707383188075e3a5a0d23794b34aa38f8ba7e032
Eugene Popolamov <eugene.popolamov@gmail.com>
2024-10-23 02:21:59 +0500

    Fix limit for pagination

--- spec/shared/paginated_serialized_data_spec.rb
+++ spec/shared/paginated_serialized_data_spec.rb
@@ -3,6 +3,6 @@ RSpec.shared_examples "paginated serialized response" do |serializer, data_class
     get path
     expect(response).to have_http_status(:ok)
     response_body = JSON.parse(response.body)
-    expect(response_body["data"]).to eq(data_class.all.map { |obj| serializer.new(obj).serializable_hash }.map(&:stringify_keys))
+    expect(response_body["data"]).to eq(data_class.all.limit(10).map { |obj| serializer.new(obj).serializable_hash }.map(&:stringify_keys))
   end
 end
